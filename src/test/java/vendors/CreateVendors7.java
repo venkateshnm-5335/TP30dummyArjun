a3b93f5f39d0a2beb17111886652d899e72a8e06
Lenovo <Lenovo@DESKTOP-NRO38HH>
2025-12-26 17:24:33 +0530

    Vendor Created........!!!!!!

---
+++ src/test/java/vendors/CreateVendors7.java
@@ -0,0 +1,10 @@
+package vendors;
+
+import org.testng.annotations.Test;
+
+public class CreateVendors7 extends configuration.BaseClass{
+	@Test(groups = "regression")
+	public void createVendorNameTest() throws Exception {
+	System.out.println("Vendor Created");
+	}
+}
